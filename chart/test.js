d48cd35b4cf27fc3eff9e7767658426fb6ce45ae
Nicolas Fontbonne <fontbonnen@gmail.com>
2018-12-13 15:11:16 +0100

    curve

--- chart/test.js
+++ chart/test.js
@@ -1,84 +1,127 @@
-var parsedate = d3.time.format("%Y-%B");
-
-var canvas = document.querySelector("#chart"),
-    context = canvas.getContext("2d");
-
-var	margin = {top: 30, left: 20, bottom: 30, right: 20},
+var	margin = {top: 20, right: 20, bottom: 110, left: 40},
+    margin2 = {top: 430, right: 20, bottom: 30, left: 40},
     width = document.getElementById('chart').clientWidth- margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
+    height2 = 400 - margin2.top - margin2.bottom;
 
-var x = d3.scaleUtc()
-    .range([0, width]);
+var svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+var parseDate = d3.timeParse("%Y-%B");
 
-var y = d3.scaleLinear()
-    .range([height, 0]);
+var x = d3.scaleTime().range([0, width]),
+    x2 = d3.scaleTime().range([0, width]),
+    y = d3.scaleLinear().range([height, 0]),
+    y2 = d3.scaleLinear().range([height2, 0]);
 
-var symbol = d3.symbol()
-    .context(context);
+var xAxis = d3.axisBottom(x),
+    xAxis2 = d3.axisBottom(x2),
+    yAxis = d3.axisLeft(y);
 
-var line = d3.line()
+var brush = d3.brushX()
+    .extent([[0, 0], [width, height2]])
+    .on("brush end", brushed);
+
+var zoom = d3.zoom()
+    .scaleExtent([1, Infinity])
+    .translateExtent([[0, 0], [width, height]])
+    .extent([[0, 0], [width, height]])
+    .on("zoom", zoomed);
+
+var area = d3.area()
+    .curve(d3.curveMonotoneX)
     .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.count); })
-    .curve(kernelSmooth, 50, 100)
-    .context(context);
+    .y0(height)
+    .y1(function(d) { return y(d.count); });
 
-context.translate(margin.left, margin.top);
+var area2 = d3.area()
+    .curve(d3.curveMonotoneX)
+    .x(function(d) { return x2(d.date); })
+    .y0(height2)
+    .y1(function(d) { return y2(d.count); });
 
-d3.requestCsv("../data/test.csv", function(d) {
-  d.date = parseTime(d.date);
-  d.count = +d.count;
-  return d;
-}, function(error, data) {
+svg.append("defs").append("clipPath")
+    .attr("id", "clip")
+  .append("rect")
+    .attr("width", width)
+    .attr("height", height);
+
+var focus = svg.append("g")
+    .attr("class", "focus")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var context = svg.append("g")
+    .attr("class", "context")
+    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+d3.csv("data/test.csv", type, function(error, data) {
   if (error) throw error;
 
-  x.domain(d3.extent(data, function(d) { return d.date; })).nice(d3.utcDay);
+  x.domain(d3.extent(data, function(d) { return d.date; }));
   y.domain([0, d3.max(data, function(d) { return d.count; })]);
+  x2.domain(x.domain());
+  y2.domain(y.domain());
 
-  context.globalAlpha = 0.5;
-  context.lineWidth = 1;
-  context.strokeStyle = "steelblue";
-  data.forEach(function(d) {
-    context.save();
-    context.translate(x(d.date), y(d.count));
-    context.beginPath();
-    symbol(d);
-    context.stroke();
-    context.restore();
-  });
+  focus.append("path")
+      .datum(data)
+      .attr("class", "area")
+      .attr("d", area);
 
-  context.beginPath();
-  line(data);
-  context.globalAlpha = 1;
-  context.lineWidth = 2;
-  context.strokeStyle = "black";
-  context.stroke();
+  focus.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  focus.append("g")
+      .attr("class", "axis axis--y")
+      .call(yAxis);
+
+  context.append("path")
+      .datum(data)
+      .attr("class", "area")
+      .attr("d", area2);
+
+  context.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height2 + ")")
+      .call(xAxis2);
+
+  context.append("g")
+      .attr("class", "brush")
+      .call(brush)
+      .call(brush.move, x.range());
+
+  svg.append("rect")
+      .attr("class", "zoom")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .call(zoom);
 });
 
-function epanechnikov(u) {
-  return (u *= u) <= 1 ? 0.75 * (1 - u) : 0;
+function brushed() {
+  if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+  var s = d3.event.selection || x2.range();
+  x.domain(s.map(x2.invert, x2));
+  focus.select(".area").attr("d", area);
+  focus.select(".axis--x").call(xAxis);
+  svg.select(".zoom").call(zoom.transform, d3.zoomIdentity
+      .scale(width / (s[1] - s[0]))
+      .translate(-s[0], 0));
 }
 
-function kernelSmooth(context, bandwidth, count) {
-  var linear = d3.curveLinear(context), x0 = Infinity, x1 = -x0, xv, yv;
-  return {
-    lineStart: function() {
-      xv = [], yv = [];
-    },
-    lineEnd: function() {
-      linear.lineStart();
-      for (var x = x0, dx = (x1 - x0) / count; x <= x1; x += dx) {
-        for (var j = 0, yi = 0, wi = 0, m = xv.length; j < m; ++j) {
-          var wji = epanechnikov((xv[j] - x) / bandwidth);
-          wi += wji, yi += yv[j] * wji;
-        }
-        linear.point(x, yi / wi);
-      }
-      linear.lineEnd();
-    },
-    point: function(x, y) {
-      xv.push(x = +x), yv.push(+y);
-      if (x < x0) x0 = x;
-      if (x > x1) x1 = x;
-    }
-  };
+function zoomed() {
+  if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
+  var t = d3.event.transform;
+  x.domain(t.rescaleX(x2).domain());
+  focus.select(".area").attr("d", area);
+  focus.select(".axis--x").call(xAxis);
+  context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
+}
+
+function type(d) {
+  d.date = parseDate(d.date);
+  d.count = +d.count;
+  return d;
 }
